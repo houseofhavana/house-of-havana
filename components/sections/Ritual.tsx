c551dacbf06bfd64b0e0e51ebaeb7c3480c33fcb
zach11z <sachithra.rash@gmail.com>
2026-02-03 16:06:08 +0530

    update content

--- components/sections/Ritual.tsx
+++ components/sections/Ritual.tsx
@@ -8,27 +8,25 @@ const Ritual = () => {
     <section className="bg-surface py-26">
       <div className="container mx-auto ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 relative">
-          <div className="p-4 md:p-12 flex flex-col justify-between gap-16 md:gap-4">
-            <div className="absolute w-[240px] h-[350px] bg-gradient-to-b from-transparent to-foreground/3 top-0 left-0"></div>
-            <div>
+          <div className="z-10 p-4 md:p-12 flex flex-col justify-between gap-16 md:gap-4">
+            <div className="z-0 absolute w-[240px] h-[350px] bg-gradient-to-b from-transparent to-foreground/3 top-0 left-0"></div>
+            <div className="z-10">
               <h3 className="heading-4-italic mb-2">Elevating</h3>
               <h2 className="heading-2 mb-4 text-primary">Men's Grooming</h2>
               <p className="text-foreground/50">
                 At House of Havana, barbering is practiced as a disciplined craft. Every cut is structured with deliberate control — calibrated blends, engineered silhouettes, and proportion-driven detailing.
               </p>
-              <p className="text-foreground/50 mt-2">
-                We do not guess.<br />
-                We assess, design, and execute.
+              <p className="text-foreground/50 mt-2 font-heading italic">
+                We do not guess. We assess, design, and execute.
               </p>
               <p className="text-foreground/50 mt-2">
                 From precise skin fades to sculpted beards and tailored finishing protocols, each service is built around technical accuracy and refined presentation.
               </p>
-              <p className="text-foreground/50 mt-2">
-                Excellence is not an upgrade here.<br />
-                It is the baseline.
+              <p className="text-foreground/50 mt-2 font-heading italic">
+                Excellence is not an upgrade here. It is the baseline.
               </p>
             </div>
-            <div className="w-full grid grid-cols-2 gap-4">
+            <div className="z-10 w-full grid grid-cols-2 gap-4">
               <div className="flex flex-col">
                 <h2>20+</h2>
                 <p className="heading-6-italic">Years of Experience</p>
