714c8af7b0b50dc6af81452e274dbb22a4e3d790
houseofhavana <house.of.havanadb@gmail.com>
2026-03-31 20:44:16 -0600

    Create socials.ts

---
+++ app/contact/data/socials.ts
@@ -0,0 +1,5 @@
+export const SOCIALS = {
+  instagram: "https://www.instagram.com/houseofhavana.ca/",
+  facebook: "https://www.facebook.com/HouseOfHavanaBarbershop/",
+  tiktok: "https://www.tiktok.com/@houseofhavana.ca",
+};
