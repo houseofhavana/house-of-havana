adfd7fa1f0021d9aed3a2075faf3fda1b68b20a7
houseofhavana <house.of.havanadb@gmail.com>
2026-06-02 21:40:59 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,36 +101,35 @@ export default function RootLayout({
         <Footer />
 
         {/* Meta Pixel noscript */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=767720532971494&ev=PageView&noscript=1"
-            alt="Meta Pixel"
-          />
-        </noscript>
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=1226872929396380&ev=PageView&noscript=1"
+    alt="Meta Pixel"
+  />
+</noscript>
 
-        {/* Meta Pixel */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '767720532971494');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-
+{/* Meta Pixel */}
+<Script
+  id="meta-pixel"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1226872929396380');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
         {/* Google Ads gtag.js */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17681636726"
