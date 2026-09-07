1339dcf5b4c380ff4712422e91cdce152ed4a931
houseofhavana <house.of.havanadb@gmail.com>
2026-09-07 11:08:43 -0600

    Create page.tsx

---
+++ app/app/drinks/page.tsx
@@ -0,0 +1,10 @@
+export default function DrinksPage() {
+  return (
+    <main>
+      <h1>House of Havana</h1>
+      <h2>Bar Menu</h2>
+
+      <p>Grooming. Conversation. A proper drink.</p>
+    </main>
+  );
+}
